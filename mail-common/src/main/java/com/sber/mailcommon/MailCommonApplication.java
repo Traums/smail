b9ec2a534d5968fa42c99e5386562a9df865e23e
Traums <42118465+Traums@users.noreply.github.com>
2024-07-27 22:01:45 +0300

    Initial

---
+++ mail-common/src/main/java/com/sber/mailcommon/MailCommonApplication.java
@@ -0,0 +1,13 @@
+package com.sber.mailcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailCommonApplication.class, args);
+    }
+
+}
